3f004e202cb78ff8e1e3ad69d88a08b4c257791d
Hasan Khan <hasanw.khan04@gmail.com>
2024-03-23 17:44:33 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Layout from './components/Layout'
+import Portfolio from './components/Portfolio'
+import Work from './components/Work'
+import Tech from './components/About/Tech'
+import Dev from './components/About/Dev'
+import './App.scss'
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="about/tech" element={<Tech />} />
+          <Route path="about/dev" element={<Dev />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="work" element={<Work />} />
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
