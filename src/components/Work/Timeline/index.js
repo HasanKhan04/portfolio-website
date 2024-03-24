b3243735c753234641744d83ed5a4a7f3f0a5dba
Hasan Khan <hasanw.khan04@gmail.com>
2024-03-23 21:23:14 -0400

    update work section

--- src/components/Work/Timeline/index.js
+++ src/components/Work/Timeline/index.js
@@ -5,7 +5,19 @@ const Timeline = () => {
   const [events, setEvents] = useState([
     {
       date: 'May-September 2023',
-      title: 'TRC Consulting, Inc.',
+      title: 'Department of National Defence',
+      role: 'Software Engineering Intern',
+      description: [
+        'Designed <b>30+ AWS Lambda Functions in AWS</b>, strategically leveraging <b>Lambda layers</b> to increase processing speed by <b>35% </b>and re-usability by <b>52%</b>, resulting in cost savings of <b>$1,000</b> per large scale deployment',
+        'Implemented <b>15+</b> serverless workflows using <b>AWS Step Functions</b>, seamlessly integrating <b>REST APIs </b>and rigorously tested with <b>Postman</b>, resulting in a <b>65%</b> reduction in execution time and enhanced operational efficiency',
+        'Crafted a <b>Spring Boot</b> application with a <b>Drools</b> logic engine, containerized it using <b>Docker</b>, and seamlessly deployed it on <b>AWS</b> using <b>EC2</b>, strategically allocating cloud resources to ensure enhanced scalability and reliability',
+      ],
+      expanded: false,
+    },
+    {
+      date: 'May-September 2023',
+      title: 'University of Waterloo',
+      role: 'Software Engineering Intern',
       description: [
         'Designed <b>30+ AWS Lambda Functions in AWS</b>, strategically leveraging <b>Lambda layers</b> to increase processing speed by <b>35% </b>and re-usability by <b>52%</b>, resulting in cost savings of <b>$1,000</b> per large scale deployment',
         'Implemented <b>15+</b> serverless workflows using <b>AWS Step Functions</b>, seamlessly integrating <b>REST APIs </b>and rigorously tested with <b>Postman</b>, resulting in a <b>65%</b> reduction in execution time and enhanced operational efficiency',
@@ -15,7 +27,8 @@ const Timeline = () => {
     },
     {
       date: 'May-September 2022',
-      title: 'Government Of Canada',
+      title: 'Adastra Corporation',
+      role: 'Software Engineering Intern',
       description: [
         'Automated documentation on <b>Azure</b> by constructing a custom <b>Python pipeline</b> to convert PDF and word files to Markdown syntax, saving <b>95%</b> of wiki writing time',
         'Engineered <b>Python</b> code to efficiently extract and process <b>100+</b> codes from a <b>data lake</b>, seamlessly integrating them into a <b>pipeline</b> for subsequent extraction and analysis, streamlining data workflows and enhancing data utilization',
@@ -48,6 +61,7 @@ const Timeline = () => {
           <div className="timeline-marker"></div>
           <div className="timeline-content">
             <h2>{event.title}</h2>
+            <h3>{event.role}</h3>
             {event.expanded ? (
               <div>
                 {event.description.map((desc, descIndex) => (
